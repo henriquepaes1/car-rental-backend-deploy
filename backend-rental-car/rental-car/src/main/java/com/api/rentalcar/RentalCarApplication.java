a2a5736165e7090a14c363b55b56ae930f5620db
Izaque Sena <izaqueseninha@gmail.com>
2022-11-10 07:43:22 -0300

    changes in DataBaseConnection and RentalCarApplicatio

--- backend-rental-car/rental-car/src/main/java/com/api/rentalcar/RentalCarApplication.java
+++ backend-rental-car/rental-car/src/main/java/com/api/rentalcar/RentalCarApplication.java
@@ -8,13 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class RentalCarApplication {
-	DatabaseConnectionCreator databaseConnectionCreator;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentalCarApplication.class, args);
-		RentalCarApplication program = new RentalCarApplication();
-		program.databaseConnectionCreator = new DatabaseConnectionCreator();
-		program.databaseConnectionCreator.createConnection();
+		DatabaseConnectionCreator.createConnection();
 	}
 
 
